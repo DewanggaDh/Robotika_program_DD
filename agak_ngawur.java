390917853c1e309c0db9aadc54a6bf65b484f2fb
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2022-12-07 10:41:07 +0700

    Update agak_ngawur.java

--- agak_ngawur.java
+++ agak_ngawur.java
@@ -13,6 +13,8 @@ import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.SensorModes;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.Color;
 import lejos.robotics.ColorDetector;
@@ -24,177 +26,364 @@ public class testtremaux2 {
 	RegulatedMotor motorD;
 	EV3ColorSensor eyesensor;
 	EV3UltrasonicSensor earsensor;
-	float[] sample;
+	EV3TouchSensor button;
+	
+	SampleProvider range;
+	SampleProvider touch;
 	
 	public void moveForward() {
-		RegulatedMotor[] motors = {this.motorD};
-		for (int i = 0; i <= 614; i++) {
-			this.motorA.synchronizeWith(motors);
-			this.motorA.startSynchronization();
-			this.motorA.rotate(614);
-			this.motorD.rotate(614);
-			this.motorA.endSynchronization();
-		}
+		//RegulatedMotor[] motors = {this.motorD};
+//		for (int i = 0; i <= 307; i++) {
+//			this.motorA.synchronizeWith(motors);
+//			this.motorA.startSynchronization();
+//			this.motorA.rotate(307);
+//			this.motorD.rotate(307);
+//			this.motorA.endSynchronization();
+//		}
+		this.motorA.forward();
+		this.motorD.forward();
 		
 	}
 	
 	public void moveBackward() {
 		//Delay.msDelay(delay);
 		RegulatedMotor[] motors = {this.motorD};
-		for (int i = 0; i <= 614; i++) {
+		for (int i = 0; i <= 307; i++) {
 			this.motorA.synchronizeWith(motors);
 			this.motorA.startSynchronization();
-			this.motorA.rotate(-614);
-			this.motorD.rotate(-614);
-			this.motorA.endSynchronization();
-		}
-	}
-	
-	public void moveLeft() {
-//		this.motorA.rotate(-180);
-//		this.motorD.rotate(180);
-		//Delay.msDelay(delay);
-		RegulatedMotor[] motors = {this.motorD};
-		for (int i = 0; i <= 180; i++) {
-			this.motorA.synchronizeWith(motors);
-			this.motorA.startSynchronization();
-			this.motorA.rotate(-180);
-			this.motorD.rotate(180);
+			this.motorA.rotate(-307);
+			this.motorD.rotate(-307);
 			this.motorA.endSynchronization();
 		}
 	}
 	
 	public void moveRight() {
-		
+//		this.motorA.rotate(-180);
+//		this.motorD.rotate(180);
+		//Delay.msDelay(delay);
 		RegulatedMotor[] motors = {this.motorD};
-		for (int i = 0; i <= 180; i++) {
+		for (int i = 0; i <= 90; i++) {
 			this.motorA.synchronizeWith(motors);
 			this.motorA.startSynchronization();
-			this.motorA.rotate(180);
-			this.motorD.rotate(180);
+			this.motorA.rotate(-90);
+			this.motorD.rotate(90);
+			this.motorA.endSynchronization();
+		}
+	}
+	
+	public void moveLeft() {
+		
+		RegulatedMotor[] motors = {this.motorD};
+		for (int i = 0; i <= 90; i++) {
+			this.motorA.synchronizeWith(motors);
+			this.motorA.startSynchronization();
+			this.motorA.rotate(90);
+			this.motorD.rotate(-90);
 			this.motorA.endSynchronization();
 		}
 		//Delay.msDelay(delay);
 	}
 	
-	public testtremaux2(Port motorAPort, Port motorDPort) {
+	public testtremaux2(Port motorAPort, Port motorDPort, Port sensor1Port, Port sensor2Port) {
 		this.motorA = new EV3LargeRegulatedMotor(motorAPort);
 		this.motorD = new EV3LargeRegulatedMotor(motorDPort);
+		this.earsensor = new EV3UltrasonicSensor(sensor1Port);
+		this.button = new EV3TouchSensor(sensor2Port);
 		//sensor = new EV3ColorSensor(port);
 		//setAmbientMode();
 		//setFloodLight(false);
 	}
-	public static int inside_maze(int y_1, int x_1) {
-		if(y_1 < 0 || y_1 > 0) {
+	public static int inside_maze(int y_1, int x_1, int y, int x) {
+		if(y_1 < 0 || y_1 > y) {
 			return 0;
 		}
-		if(x_1 < 0 || x_1 > 0) {
+		if(x_1 < 0 || x_1 > x) {
 			return 0;
 		}
 		return 1;
 	}
-//	public int junction() {
-//		this.moveLeft();
-//		if
-//		return 0;
-//	}
+	public float check_sound() {
+		
+		range = earsensor.getDistanceMode();
+		float[] sample = new float[range.sampleSize()];
+		range.fetchSample(sample, 0);
+		return (float)sample[0];
+	}
+	public int junction() {
+		this.moveLeft();
+		int count = 0;
+		range = earsensor.getDistanceMode();
+		float[] sample = new float[range.sampleSize()];
+		range.fetchSample(sample, 0);
+		if((int)sample[0] <= 0.05) {
+			count += 1;
+		}
+		this.moveRight();
+		this.moveRight();
+		range = earsensor.getDistanceMode();
+		//float[] sample = new float[range.sampleSize()];
+		range.fetchSample(sample, 0);
+		if((int)sample[0] <= 0.05) {
+			count += 1;
+		}
+		this.moveLeft();
+//		range = earsensor.getDistanceMode();
+//		//float[] sample = new float[range.sampleSize()];
+//		range.fetchSample(sample, 0);
+//		if((int)sample[0] <= 0.05) {
+//			count += 1;
+//		}
+		return count;
+	}
+	public int sentuh() {
+		touch = button.getTouchMode();
+		float[] touching = new float[touch.sampleSize()];
+		touch.fetchSample(touching,0);
+		return (int)touching[0];
+	}
 	public static void main(String args[]) {
 		int x = 0, y = 0;
-		testtremaux2 mC = new testtremaux2(MotorPort.A, MotorPort.D);
-		EV3UltrasonicSensor soundSensor = new EV3UltrasonicSensor(SensorPort.S1);
-		SampleProvider mode = soundSensor.getDistanceMode();
-		while(true) {
-			LCD.drawString(String.format("Lebar Maze : %d", x), 0, 0);
-			Button.UP.waitForPress();
-			x += 1;
-			if (x > 10) {
-				x = 0;
-			}
-			if(Button.ESCAPE.isDown()) {
-				break;
-			}
-			
-			LCD.clear();
-		}
-		while(true) {
-			LCD.drawString(String.format("Panjang Maze : %d", y), 0, 0);
-			Button.UP.waitForPress();
-			y += 1;
-			if (y > 10) {
-				y = 0;
-			}
-			if(Button.ESCAPE.isDown()) {
-				break;
-			}
-			LCD.clear();
-		}
-		int[][] array_maze = new int[y][x];
-//		int[][] array_maze = {{0, 0, 0, 0, 0},
-//		             {0, 0, 0, 0, 0},
-//		             {0, 0, 0, 0, 0},
-//		             {0, 0, 0, 0, 0},
-//		             {0, 0, 0, 0, 0}};
+		testtremaux2 mC = new testtremaux2(MotorPort.A, MotorPort.D, SensorPort.S1, SensorPort.S2);
+		//EV3UltrasonicSensor soundSensor = new EV3UltrasonicSensor(SensorPort.S1);
+		//SampleProvider mode = soundSensor.getDistanceMode();
+//		while(true) {
+//			LCD.drawString(String.format("Lebar Maze : %d", x), 0, 0);
+//			Button.UP.waitForPress();
+//			x += 1;
+//			if (x > 10) {
+//				x = 0;
+//			}
+//			if(Button.ESCAPE.isDown()) {
+//				break;
+//			}
+//			
+//			LCD.clear();
+//		}
+//		while(true) {
+//			LCD.drawString(String.format("Panjang Maze : %d", y), 0, 0);
+//			Button.UP.waitForPress();
+//			y += 1;
+//			if (y > 10) {
+//				y = 0;
+//			}
+//			if(Button.ESCAPE.isDown()) {
+//				break;
+//			}
+//			LCD.clear();
+//		}
+		//int[][] array_maze = new int[y][x];
+		int[][] array_maze = 	{{0, 0, 0, 0, 0},
+								{1, 1, 1, 1, 0},
+								{0, 0, 0, 0, 0},
+								{0, 1, 1, 1, 0},
+								{0, 0, 0, 1, 0}};
 		            		 
 		int prev_pos_x = -1;
 		int prev_pos_y = -1;
 		int input_x = 0;
 		int input_y = 0;
-		while(true) {
-			
-			if (input_x >= x - 1) {
-				input_x = 0;
-			}
-			LCD.drawString(String.format("Posisi x awal : %d", input_x), 0, 0);
-			Button.UP.waitForPressAndRelease();
-			input_x += 1;
-			if(Button.ESCAPE.isDown()) {
-				break;
-			}
-			LCD.clear();
-		}
-		while(true) {
-			
-			if (input_y >= y - 1) {
-				input_y = 0;
-			}
-			LCD.drawString(String.format("Posisi y awal : %d", input_y), 0, 0);
-			Button.UP.waitForPressAndRelease();
-			input_y += 1;
-			if(Button.ESCAPE.isDown()) {
-				break;
-			}
-			LCD.clear();
-		}
-		int[] starting_point = {input_y, input_x};
-		String[] posisi = {"Utara", "Timur", "Selatan", "Barat"};
-		int posisi_awal = 0;
-		while(true) {
-			
-			
-			if (posisi_awal >= 4) {
-				posisi_awal = 0;
-			}
-			LCD.drawString("Arah awal : ", 0, 0);
-			LCD.drawString(String.format("%s", posisi[posisi_awal]), 0, 2);
-			Button.UP.waitForPressAndRelease();
-			posisi_awal += 1;
-			if(Button.ESCAPE.isDown()) {
-				break;
-			}
-			LCD.clear();
-		}
+		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
+		SensorMode mode = colorSensor.getRedMode();
 		
+		LCD.drawString("white", 0, 0);
+		Button.ESCAPE.waitForPressAndRelease();
+		float[] sampleWhite = new float[1];
+		mode.fetchSample(sampleWhite, 0);;
+		LCD.drawInt(new Float(sampleWhite[0] * 100).intValue(), 2, 2);
+		Delay.msDelay(1000);
+		LCD.clear();
+		
+		Delay.msDelay(1000);
+		LCD.drawString("Black", 0, 0);
+		Button.ESCAPE.waitForPressAndRelease();
+		float[] sampleBlack = new float[1];
+		mode.fetchSample(sampleBlack, 0);
+		LCD.drawInt(new Float(sampleBlack[0] * 100).intValue(), 2, 2);
+		Delay.msDelay(1000);
+		LCD.clear();
+		Delay.msDelay(2000);
+		
+		int defaultPower = 20;
+		int multiplyingFactor = 100;
+		int multipleblack = 25;
+		float[] color = new float[1];
+		//while(!Button.ESCAPE.isDown()) {
+			
+//			if (input_x >= x - 1) {
+//				input_x = 0;
+//			}
+//			LCD.drawString(String.format("Posisi x awal : %d", input_x), 0, 0);
+//			Button.UP.waitForPressAndRelease();
+//			input_x += 1;
+//			if(Button.ESCAPE.isDown()) {
+//				break;
+//			}
+//			LCD.clear();
+//		}
+//		while(true) {
+//			
+//			if (input_y >= y - 1) {
+//				input_y = 0;
+//			}
+//			LCD.drawString(String.format("Posisi y awal : %d", input_y), 0, 0);
+//			Button.UP.waitForPressAndRelease();
+//			input_y += 1;
+//			if(Button.ESCAPE.isDown()) {
+//				break;
+//			}
+//			LCD.clear();
+//		}
+//		int[] starting_point = {input_y, input_x};
+//		String[] posisi = {"Utara", "Timur", "Selatan", "Barat"};
+//		int posisi_awal = 0;
+//		while(true) {
+//			
+//			
+//			if (posisi_awal >= 4) {
+//				posisi_awal = 0;
+//			}
+//			LCD.drawString("Arah awal : ", 0, 0);
+//			LCD.drawString(String.format("%s", posisi[posisi_awal]), 0, 2);
+//			Button.UP.waitForPressAndRelease();
+//			posisi_awal += 1;
+//			if(Button.ESCAPE.isDown()) {
+//				break;
+//			}
+//			LCD.clear();
+//		}
+		//testtremaux2 mc = new testtremaux2(MotorPort.A, MotorPort.D);
+		
+//		float suara = mC.check_sound();
+//		LCD.drawString(String.format("Jarak (m) = %.2f", suara), 0, 0);
+//		if(suara <= 0.05) {
+//			mC.moveLeft();
+//		}
+//		else {
+//			mC.moveForward();
+//		}
+		//mC.moveRight();
+		//mC.moveLeft();
+//		RegulatedMotor[] motors = {mC.motorD};
+//		for (int i = 0; i <= 90; i++) {
+//			mC.motorA.synchronizeWith(motors);
+//			mC.motorA.startSynchronization();
+//			mC.motorA.rotate(-90);
+//			mC.motorD.rotate(90);
+//			mC.motorA.endSynchronization();
+//		}
+//		for (int i = 0; i <= 90; i++) {
+//			mC.motorA.synchronizeWith(motors);
+//			mC.motorA.startSynchronization();
+//			mC.motorA.rotate(90);
+//			mC.motorD.rotate(-90);
+//			mC.motorA.endSynchronization();
+//		}
 		//test motor
-//		mC.moveForward();
-//		mC.moveBackward();
-//		mC.moveLeft();
-//		mC.moveRight();
-		int tempe = inside_maze(starting_point[0], starting_point[1]);
-		EV3LargeRegulatedMotor largeMotorkiri = new EV3LargeRegulatedMotor(MotorPort.A);
-		EV3LargeRegulatedMotor largeMotorkanan = new EV3LargeRegulatedMotor(MotorPort.D);
+//		while(mC.check_sound() >= 0.1) {
+//			mC.moveForward();
+//		}
+		//mC.moveBackward();
+		//mC.moveLeft();
+		//mC.moveRight();
+		//int tempe = inside_maze(starting_point[0], starting_point[1], y, x);
 		
-		while(inside_maze(starting_point[0], starting_point[1]) != 0){
-			if
+		
+		//while(inside_maze(starting_point[0], starting_point[1], y, x) != 0){
+		EV3GyroSensor arah = new EV3GyroSensor(SensorPort.S3);
+		SampleProvider empat = arah.getAngleMode();
+		float[] four = new float[empat.sampleSize()];
+		
+		
+		
+		while(!Button.ESCAPE.isDown()) {
+			empat.fetchSample(four, 0);
+			LCD.drawString(String.format("White = %.2f",  four[0]), 0, 0);
+//			float avgLight = (sampleBlack[0] + sampleWhite[0]) / 2;
+//			mode.fetchSample(color, 0);
+//			
+//			LCD.drawString(String.format("White = %.2f", sampleWhite[0] * 100), 0, 0);
+//			LCD.drawString(String.format("Black = %.2f", sampleBlack[0] * 100), 0, 1);
+//			LCD.drawString(String.format("AverageLight = %.2f", avgLight), 0, 2);
+//			
+//			
+//			//Backup plan
+//			if(avgLight < color[0]) {
+//				mC.motorA.stop(true);
+//				mC.motorD.forward();
+//			}
+//			else if(avgLight > color[0]) {
+//				mC.motorA.stop(true);
+//				mC.motorD.forward();
+//			}
+//			else {
+//				mC.motorA.forward();
+//				mC.motorD.forward();
+//			}
+//			empat.fetchSample(four, 0);
+		}
+			
+//		while(mC.check_sound() >= 0.1) {
+//			//if far wall jalan
+//			//if near wall, stop
+//			//move left
+//			//check
+//			//if wall
+//				//move to right 2x
+//				//if wall
+//					//moev right
+//					//move forward once
+//				//else
+//					//move
+//			//else
+//				//move
+//			
+//		}
+		
+//		while(!Button.DOWN.isDown()){
+//			if(mC.junction() != 0) {
+//				mC.moveLeft();
+//				if(mC.check_sound() <= 0.05) {
+//					mC.moveRight();
+//					if(mC.check_sound() <= 0.05) {
+//						mC.moveRight();
+//						if(mC.check_sound() <= 0.05) {
+//							mC.moveLeft();
+//							mC.moveBackward();
+//							break;
+//						}
+//						else {
+//							mC.moveForward();
+//						}
+//					}
+//					else {
+//						mC.moveForward();
+//					}
+//				}
+//				else {
+//					mC.moveForward();
+//				}
+//				
+//			}
+//			else {
+//				mC.moveForward();
+//			}
+		
+		
+			//bonker method
+//			if(mC.check_sound() <= 0.05) {
+//				mC.moveLeft();
+//				if(mC.check_sound() <= 0.05) {
+//					mC.moveRight();
+//					mC.moveRight();
+//					if(mC.check_sound() <= 0.05) {
+//						mC.moveLeft();
+//						mC.moveBackward();
+//					}
+//				}
+//			}
+//			else {
+//				mC.moveForward();
+//			}
 		}
 //			if(junction() == True) {
 //				if(forward != wall || forward != mark) {
@@ -226,4 +415,4 @@ public class testtremaux2 {
 //		
 //		}
 	}
-}
+//}
